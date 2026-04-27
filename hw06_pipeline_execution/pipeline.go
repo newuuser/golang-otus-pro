0d54f4445ff0fea5c94102d4e1b8510e3bd88ba3
Golianskii Daniil <git@github.com>
2026-04-27 23:35:51 +0800

    Hw1 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,42 @@ type (
 
 type Stage func(in In) (out Out)
 
+// без : n stage первым обрабатывает done, n-1 пробует передать -> deadlock
+func drain(in Out) {
+	for range in {
+	}
+}
+
+// wraps in chan + done chan to in chan that closes at done signal
+func gen(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				drain(in)
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					drain(in)
+					return
+				}
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = gen(in, done)
+		in = stage(in)
+	}
+	return in
 }
