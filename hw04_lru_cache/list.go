fcc848d862c89a1557a1e2d488c9550175c40b35
Golianskii Daniil <git@github.com>
2026-05-11 16:14:22 +0800

    separate hw's

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,79 +17,10 @@ type ListItem struct {
 }
 
 type list struct {
-	size int
-	end  *ListItem
-	beg  *ListItem
-}
-
-func (l *list) Len() int {
-	return l.size
-}
-
-func (l *list) Front() *ListItem {
-	return l.beg
-}
-func (l *list) Back() *ListItem {
-	return l.end
-}
-
-func (l *list) PushFront(v interface{}) *ListItem {
-	li := &ListItem{Value: v, Next: l.Front(), Prev: nil}
-	if l.Len() > 0 {
-		l.Front().Prev = li
-	} else {
-		l.end = li
-	}
-	l.beg = li
-	l.size++
-	return li
-}
-
-func (l *list) PushBack(v interface{}) *ListItem {
-	li := &ListItem{Value: v, Next: nil, Prev: l.Back()}
-	if l.Len() > 0 {
-		l.Back().Next = li
-	} else {
-		l.beg = li
-	}
-	l.end = li
-	l.size++
-	return li
-}
-
-func (l *list) Remove(i *ListItem) {
-	// messy, maybe redo
-	if i == l.Front() {
-		l.beg = l.beg.Next
-	}
-	if i == l.Back() {
-		l.end = l.end.Prev
-		if l.end != nil {
-			l.end.Next = nil
-		}
-		return
-	}
-	if i.Next != nil {
-		i.Next.Prev = i.Prev
-	}
-	if i.Prev != nil {
-		i.Prev.Next = i.Next
-	}
-	l.size--
-}
-
-func (l *list) MoveToFront(i *ListItem) {
-	if i == l.Front() {
-		return
-	}
-	l.Remove(i)
-	l.size++
-
-	i.Prev = nil
-	i.Next = l.beg
-	l.beg = i
+	List // Remove me after realization.
+	// Place your code here.
 }
 
 func NewList() List {
-	return &list{size: 0, end: nil, beg: nil}
+	return new(list)
 }
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,47 +9,7 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-
 func Run(tasks []Task, n, m int) error {
-	// m<=0 : ignore errors
-	if m <= 0 {
-		m = len(tasks) + 1
-	}
-
-	var mu sync.RWMutex
-	stack := make(chan struct{}, n)
-	var wg sync.WaitGroup
-	ctr := 0
-
-	for i := 0; i < len(tasks); i++ {
-		mu.RLock()
-		if ctr >= m {
-			mu.RUnlock()
-			break
-		}
-		mu.RUnlock()
-
-		j := i
-		stack <- struct{}{}
-		wg.Add(1)
-		go func() {
-			defer func() {
-				<-stack
-				wg.Done()
-			}()
-			err := tasks[j]()
-			if err != nil {
-				mu.Lock()
-				ctr++
-				mu.Unlock()
-			}
-		}()
-	}
-
-	wg.Wait()
-	if ctr < m {
-		return nil
-	} else {
-		return ErrErrorsLimitExceeded
-	}
+	// Place your code here.
+	return nil
 }
