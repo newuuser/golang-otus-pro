24b633c9aefe4dcc4387157071e96eb4b6635cbe
Golianskii Daniil <git@github.com>
2026-05-23 03:13:21 +0800

    tests finished

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -8,18 +8,17 @@ import (
 
 func TestRunCmd(t *testing.T) {
 	t.Run("Valid", func(t *testing.T) {
-		//ini := []string{"ls"}
-		ini := []string{"./testdata/good_program/good_program.exe"}
+		ini := []string{"go", "run", "./testdata/good_program/main.go"}
 		code := RunCmd(ini, make(Environment))
 		require.Zero(t, code)
 	})
 	t.Run("Invalid", func(t *testing.T) {
-		ini := []string{"./testdata/failing_program/failing_program.exe"}
+		ini := []string{"go", "run", "./testdata/failing_program/main.go"}
 		code := RunCmd(ini, make(Environment))
 		require.Equal(t, 1, code)
 	})
 	t.Run("Testing environment", func(t *testing.T) {
-		ini := []string{"./testdata/testing_program/testing_program.exe", "arg1=1", "arg2=2"}
+		ini := []string{"go", "run", "./testdata/testing_program/main.go", "arg1=1", "arg2=2"}
 		env, _ := ReadDir("./testdata/env")
 		code := RunCmd(ini, env)
 		require.Equal(t, 0, code)
