0a4646a947ee042a90e28606562eea9d0626f222
Golianskii Daniil <git@github.com>
2026-06-20 21:01:39 +0800

    rm hw10

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io"
-	"net"
 	"time"
 )
 
@@ -13,37 +12,9 @@ type TelnetClient interface {
 	Receive() error
 }
 
-type ClientImpl struct {
-	address string
-	conn    net.Conn
-	timeout time.Duration
-	in      io.ReadCloser
-	out     io.Writer
-}
-
-func (c *ClientImpl) Connect() error {
-	return nil
-}
-
-func (c *ClientImpl) Send() error {
-	return nil
-}
-
-func (c *ClientImpl) Receive() error {
-	return nil
-}
-
-func (c *ClientImpl) Close() error {
-	return nil
-}
-
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	return &ClientImpl{
-		address: address,
-		timeout: timeout,
-		in:      in,
-		out:     out,
-	}
+	// Place your code here.
+	return nil
 }
 
 // Place your code here.
