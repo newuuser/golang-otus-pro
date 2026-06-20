204fa1ec8f7fbbd8bce1ade698b8d5ec4bfc37ff
Golianskii Daniil <git@github.com>
2026-06-21 00:29:46 +0800

    done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -6,9 +6,60 @@ type (
 	Bi  = chan interface{}
 )
 
+func drain(in In) {
+	go func() {
+		for {
+			select {
+			case _, ok := <-in:
+				if !ok {
+					return
+				}
+			}
+		}
+	}()
+}
+
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	ch := make(Bi)
+	var res In = ch
+
+	for _, stage := range stages {
+		res = stage(res)
+	}
+
+	go func() {
+		defer close(ch)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				ch <- v
+			}
+		}
+	}()
+	ret := make(Bi)
+
+	go func() {
+		defer close(ret)
+		for {
+			select {
+			case <-done:
+				drain(res)
+				return
+			case v, ok := <-res:
+				if !ok {
+					return
+				}
+				ret <- v
+			}
+		}
+	}()
+
+	return ret
 }
